2826e36b7f25724bc8b37636ebb949e784848c5a
Michael <200~pregnant-mutt-cape@duck.com>
2025-03-17 11:37:45 -0500

    updated to be more clear

--- arrays.js
+++ arrays.js
@@ -8,12 +8,14 @@ let mixedArray = ["Bob", 35, false, {city: "Houston"}, ["gaming", "cooking", "cl
 console.log(mixedArray[0]);
 console.log(mixedArray[4][1]);
 
-// update age in array and add a new hobby
+// update age in array and add a new hobby using .push
 
-let ageUpdate = mixedArray.fill("Chuck", 0, 1);
-
-console.log(ageUpdate);
+mixedArray.fill("Chuck", 0, 1);
 
 mixedArray[4].push("eating");
+
+
+
+// log updated array
 
 console.log(mixedArray);
